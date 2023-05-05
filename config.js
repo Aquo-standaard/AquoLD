8644de6f23d2c19c04ec6151da49af0e0b4f95a9
Eclidian <w.vanriel@ihw.nl>
2023-05-05 09:59:35 +0200

    initiele bestanden publicatie

---
+++ config.js
@@ -0,0 +1,37 @@
+var respecConfig = {
+  specStatus: "",
+  specType: "IM",
+  //subtitle: "IMWA gegevenscatalogus",
+   //publishDate: "2023-04-26", 
+  logos: [
+  {src: 'https://www.aquo.nl//logos/aquo_logo.svg',
+    href: "http://aquo.nl",
+    alt: "Aquo",
+    width: 252,
+    height: 130,
+    id: 'AWK'},
+  {
+    src: 'https://cuatro.sim-cdn.nl/ihw/uploads/styles/logo/media/ihw_logo_def_taglines.png?cb=ZxTlKJiY',
+    href: "https://www.ihw.nl",
+    alt: "Informatiehuis Water",
+    width: 229,
+    height: 130,
+    id: 'IHW',
+  }]  ,
+  editors: [],
+  authors: [
+    {
+      name: "Informatiehuis Water",
+	  mailto: "servicedesk@ihw.nl",
+	  note: "033 203 50 97",
+      companyURL: "http://www.ihw.nl",
+    }],
+  shortName: "AWK",
+  pubDomain: "IMWA",
+  github: "https://github.com/Aquo-standaard/AWK",
+  previousPublishDate: "2023-04-26",
+  previousStatus: "AQ-WV",
+  localBiblio: {
+    
+    },
+};
